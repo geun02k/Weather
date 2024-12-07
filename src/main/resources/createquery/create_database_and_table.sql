6218dbfbff700b86ae5878c8858a060084e77bdb
geun02k <geun02k@gmail.com>
2024-12-08 08:03:54 +0900

    날씨일기저장API - diary 테이블 생성

--- src/main/resources/createquery/create_database_and_table.sql
+++ src/main/resources/createquery/create_database_and_table.sql
@@ -10,4 +10,15 @@ create table memo(
     text VARCHAR(50) NOT NULL
 );
 
+-- diary 테이블생성
+create table diary(
+    id INT not null primary key auto_increment,
+    weather VARCHAR(50) NOT NULL,
+    icon VARCHAR(50) NOT NULL,
+    temperature DOUBLE NOT NULL,
+    text VARCHAR(500) NOT NULL,
+    date DATE NOT NULL
+);
+
+
 
